2f32540263f39a924965cc06c94354e4dc5e45d5
ayush110404 <ayushagarwal1102004@gmail.com>
2025-07-30 13:08:04 +0530

    Refactor caching logic to improve cache hit handling and response serving

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 import express from 'express';
 import fs from 'fs';
 import mime from 'mime-types'
+import path from 'path';
 
 
 const app = express();
 export default async function startProxyServer(options){
     app.use(async (req, res, next) => {
-        // console.log(`Received request for ${req.url} with method ${req.method}`);
-        // console.log(`Headers:`, req.headers);
-        // console.log(`Query params:`, req.query);
         let requestRoute = `./cache${req.path}`;
         console.log(`Fetching from origin: ${options.origin + req.path}`);
         if(fs.existsSync(requestRoute)) {
             const cacheFilePath = fs.readdirSync(requestRoute)[0];
-            const content = fs.readFileSync(`${requestRoute}/${cacheFilePath}`, 'utf-8');
-            console.log(`Cache hit for ${req.url}`);
-            res.send(content);
+            const absolutePath = path.resolve(`${requestRoute}/${cacheFilePath}`);
+            console.log(`Cache miss for ${req.url}`);
+            res.setHeader('X-Cache', 'HIT');
+            res.sendFile(absolutePath);
             return;
         }else{
             fs.mkdirSync(requestRoute, { recursive: true });
@@ -25,14 +24,23 @@ export default async function startProxyServer(options){
                 res.status(response.status).send(`Error fetching from origin: ${response.statusText}`);
                 return;
             }
-            const data = await response.json();
+            const data = Buffer.from(await response.arrayBuffer());
             console.log(`Fetching data: ${response.url}, type: ${response.headers.get('content-type')} , res: ${response}`);
             const fileType = mime.extension(response.headers.get('content-type')) || 'txt';
             requestRoute += `/response${Date.now()}.${fileType}`;
+            const absolutePath = path.resolve(requestRoute);
             console.log(`Serving cached response for ${req.url}`);
-            fs.writeFileSync(requestRoute, JSON.stringify(data));
+            fs.writeFileSync(absolutePath, data);
             console.log(`Cache miss for ${req.url}, writing to cache.`);
-            res.send(`Cached response for ${req.url} at ${new Date().toISOString()}`);
+            let header={};
+            response.headers.forEach((value, key) => {
+                if(key !== 'content-encoding' && key != 'transfer-encoding' && key != 'content-length') {
+                    header[key] = value;
+                }
+            });
+            res.set(header);
+            res.setHeader('X-Cache', 'MISS');
+            res.sendFile(absolutePath);
             return;
         }
     });
